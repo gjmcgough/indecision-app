0176c85eda562de396dbf33e0b6f3658c488d997
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-10 16:59:56 -0800

    es6 sandbox

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,22 @@
+// const square = function(x) {
+//   return x * x;
+// };
+//
+// console.log(square(8));
+//
+// // const sq = (x) => {
+// //   return x + x;
+// // };
+//
+// const sq = (x) => x - x;
+//
+// console.log(sq(2));
+
+
+// const getFirstName = (x) => {
+//   return x.split(' ')[0];
+// };
+
+const getFirstName = (x) => x.split(' ')[0];
+
+console.log(getFirstName('Mike Smith'));
--- src/playground/es6-arrow-functions-2.js
+++ src/playground/es6-arrow-functions-2.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // arguments no longer bound example
 
 // const add = (a, b) => {
@@ -11,15 +9,11 @@
 
 // this no longer bound example
 
-var user = {
+const user = {
   name: "Greg",
   cities: ["Chicago", "Atlanta", "San Diego"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + " has lived in " + city;
-    });
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + " has lived in " + city);
 
     // console.log(this.name);
     // console.log(this.cities);
@@ -32,18 +26,15 @@ var user = {
 
 console.log(user.printPlacesLived());
 
-var multiplier = {
+
+const multiplier = {
   //numbers - array of numbers to multiply
-  numbers: [10, 20, 30],
+  numbers: [10,20,30],
   // multiplyBy - single number
   multiplyBy: 3,
   // multiply - new array where numbes have been multiplied
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (numNum) {
-      return _this2.multiplyBy * numNum;
-    });
+  multiply() {
+    return this.numbers.map((numNum) => this.multiplyBy * numNum)
   }
 };
 

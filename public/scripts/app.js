b427e151aba01c7fbe7e3a3e39029c57764e3d9a
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-13 13:05:44 -0800

    complete visibility build it challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,95 +1,40 @@
-"use strict";
+'use strict';
 
 var app = {
-  title: "Indecision App",
-  subTitle: "Put your life in the hands of a computer!!!",
-  options: []
-};
-
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    renderIndecisionApp();
-  }
-};
-
-var resetForm = function resetForm(e) {
-  e.preventDefault();
-  app.options = [];
-  renderIndecisionApp();
+  title: "Visibility Toggle"
 };
 
 var appRoot = document.getElementById('app');
 
-// const numbers = [55,101,1000];
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
+var visible = 0;
+var setVisible = function setVisible(e) {
+  e.preventDefault();
+  visible === 0 ? visible = 1 : visible = 0;
+  renderApp();
 };
 
-var renderIndecisionApp = function renderIndecisionApp() {
+var renderApp = function renderApp() {
   var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-      "h1",
+      'h1',
       null,
       app.title
     ),
-    app.subTitle && React.createElement(
-      "p",
+    React.createElement(
+      'button',
+      { onClick: setVisible },
+      'Show dem DEETS'
+    ),
+    visible === 0 ? '' : React.createElement(
+      'p',
       null,
-      app.subTitle
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? 'Here are your options' : 'No options.'
-    ),
-    React.createElement(
-      "button",
-      { disabled: app.options.length <= 0, onClick: onMakeDecision },
-      "Where should I eat?"
-    ),
-    React.createElement("br", null),
-    React.createElement("br", null),
-    React.createElement(
-      "button",
-      { onClick: resetForm },
-      "Reset options"
-    ),
-    React.createElement(
-      "ol",
-      null,
-      app.options.map(function (option) {
-        return React.createElement(
-          "li",
-          { key: option },
-          "Option: ",
-          option
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add Option"
-      )
+      'These are your deets!'
     )
   );
 
   ReactDOM.render(template, appRoot);
 };
 
-renderIndecisionApp();
+renderApp();
---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,28 @@
+const app = {
+  title: "Visibility Toggle"
+}
+
+let appRoot = document.getElementById('app');
+
+let visible = 0;
+const setVisible = (e) => {
+  e.preventDefault();
+  (visible === 0) ? visible = 1 : visible = 0;
+  renderApp();
+}
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      <button onClick={setVisible}>Show dem DEETS</button>
+      {
+        (visible === 0) ? '' : <p>These are your deets!</p>
+      }
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+renderApp();

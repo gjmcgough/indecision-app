49df03d4126ffdbd68d7e9ae5cb53a48edf9f491
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-13 10:58:54 -0800

    add input and form reset logic.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,47 +3,85 @@
 var app = {
   title: "Indecision App",
   subTitle: "Put your life in the hands of a computer!!!",
-  options: ['One', 'Two']
+  options: []
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    "p",
-    null,
-    app.subTitle
-  ),
-  app.options.length > 0 ? React.createElement(
-    "p",
-    null,
-    "Here are your options"
-  ) : React.createElement(
-    "p",
-    null,
-    "No options."
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "This is item one."
-    ),
-    React.createElement(
-      "li",
-      null,
-      "This is item two."
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderIndecisionApp();
+  }
+};
+
+var resetForm = function resetForm(e) {
+  e.preventDefault();
+  app.options = [];
+  renderIndecisionApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderIndecisionApp = function renderIndecisionApp() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      "p",
+      null,
+      app.subTitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options.'
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      { onClick: resetForm },
+      "Reset"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      React.createElement(
+        "li",
+        null,
+        "This is item one."
+      ),
+      React.createElement(
+        "li",
+        null,
+        "This is item two."
+      ),
+      React.createElement(
+        "form",
+        { onSubmit: onFormSubmit },
+        React.createElement("input", { type: "text", name: "option" }),
+        React.createElement(
+          "button",
+          null,
+          "Add Option"
+        )
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderIndecisionApp();
--- src/app.js
+++ src/app.js
@@ -1,21 +1,49 @@
 const app = {
   title: "Indecision App",
   subTitle: "Put your life in the hands of a computer!!!",
-  options: ['One', 'Two']
+  options: []
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    {app.options.length > 0 ? <p>Here are your options</p> : <p>No options.</p>}
-    <ol>
-      <li>This is item one.</li>
-      <li>This is item two.</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderIndecisionApp();
+  }
+};
+
+const resetForm = (e) => {
+  e.preventDefault();
+  app.options = [];
+  renderIndecisionApp();
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const renderIndecisionApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options.'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={resetForm}>Reset</button>
+      <ol>
+        <li>This is item one.</li>
+        <li>This is item two.</li>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </ol>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+renderIndecisionApp();

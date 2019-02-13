24fc088cc1d895ff92a97a332781a7ebbe6c243f
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-13 13:16:55 -0800

    complete section 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,40 @@
 "use strict";
 
 var app = {
-  title: "Visibility Toggle"
+  title: "Indecision App",
+  subTitle: "Put your life in the hands of a computer!!!",
+  options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderIndecisionApp();
+  }
+};
+
+var resetForm = function resetForm(e) {
+  e.preventDefault();
+  app.options = [];
+  renderIndecisionApp();
 };
 
 var appRoot = document.getElementById('app');
 
-var visible = false;
+// const numbers = [55,101,1000];
 
-var setVisible = function setVisible(e) {
-  e.preventDefault();
-  visible = !visible;
-  renderApp();
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
 };
 
-var renderApp = function renderApp() {
+var renderIndecisionApp = function renderIndecisionApp() {
   var template = React.createElement(
     "div",
     null,
@@ -23,19 +43,53 @@ var renderApp = function renderApp() {
       null,
       app.title
     ),
-    React.createElement(
-      "button",
-      { onClick: setVisible },
-      visible ? "Hide Deets" : "Hide Deets"
-    ),
-    visible ? React.createElement(
+    app.subTitle && React.createElement(
       "p",
       null,
-      "These are your deets!"
-    ) : ''
+      app.subTitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options.'
+    ),
+    React.createElement(
+      "button",
+      { disabled: app.options.length <= 0, onClick: onMakeDecision },
+      "Where should I eat?"
+    ),
+    React.createElement("br", null),
+    React.createElement("br", null),
+    React.createElement(
+      "button",
+      { onClick: resetForm },
+      "Reset options"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          "Option: ",
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
+    )
   );
 
   ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+renderIndecisionApp();

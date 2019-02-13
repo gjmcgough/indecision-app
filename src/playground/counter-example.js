b15a0b52d3301b7e14d978c016445c9dac644746
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-13 10:12:57 -0800

    move counter into own file

---
+++ src/playground/counter-example.js
@@ -0,0 +1,32 @@
+let count = 0;
+
+const addOne = () => {
+  count++;
+  renderCounterApp();
+};
+
+const minusOne = () => {
+  count--;
+  renderCounterApp();
+};
+
+const reset = () => {
+  count = 0;
+  renderCounterApp();
+};
+
+
+const renderCounterApp = () => {
+  const template2 = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>RESET</button>
+    </div>
+  );
+
+  ReactDOM.render(template2, appRoot);
+}
+
+renderCounterApp();

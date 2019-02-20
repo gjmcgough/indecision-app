e98d1c5e65ad315950c0c902ca9f4b5fd27f5c0f
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-20 10:33:54 -0800

    completed the basic component structure, and render app as component not jsx object

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -21,7 +34,18 @@ class Action extends React.Component {
 
 class Options extends React.Component {
   render(){
-    return <p>Options component</p>;
+    return (
+      <div>
+        <p>OptionS component</p>
+        <Option />
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render(){
+    return <p>Option Component</p>;
   }
 }
 
@@ -31,13 +55,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));

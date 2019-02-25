2d9d392c020d3a896fb4eb8f1be87a825cc20841
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-25 15:40:48 -0800

    counter lifecycle methods excercise

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,18 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+  componentDidMount() {
+    const json = localStorage.getItem('count');
+    const count = JSON.parse(json);
+
+    this.setState(() => ({ count }));
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const json = JSON.stringify(this.state.count);
+    localStorage.setItem('count', json);
   }
   handleAddOne(){
     this.setState((prevState) => {
@@ -31,17 +41,13 @@ class Counter extends React.Component {
   }
   render(){
     return(<div>
-      <h1>Count: {this.state.count}</h1>
+      <h1 name="count">Count: {this.state.count}</h1>
       <button onClick={this.handleAddOne}>+1</button>
       <button onClick={this.handleMinusOne}>-1</button>
       <button onClick={this.handleReset}>Reset</button>
     </div>
     )
   }
-}
-
-Counter.defaultProps = {
-  count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));

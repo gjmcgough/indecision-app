642e90048473fb08e0901d3407a6ca772f468d31
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-26 11:57:04 -0800

    break out components into separate files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 // install > import > use
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import validator from 'validator';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>this is jsx from webpack</p>
-
-ReactDOM.render(template, document.getElementById('app'));
-
-// console.log(validator.isEmail('test@gmail.com'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/person.js
+++
@@ -1,7 +0,0 @@
-export const isAdult = (age) => age >= 18;
-
-export const canDrink = (age) => age >= 21;
-
-export default (age) => age >= 65;
-
-// export {isAdult, canDrink};

933f3a1fa966d89fd706c2d3c9722152ccbff6aa
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-26 11:29:25 -0800

    install babel core and babel loader and configure for use in webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 // install > import > use
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import validator from 'validator';
 
-import validator from 'validator';
+const template = <p>this is jsx from webpack</p>
 
-console.log(validator.isEmail('test@gmail.com'));
+ReactDOM.render(template, document.getElementById('app'));
+
+// console.log(validator.isEmail('test@gmail.com'));

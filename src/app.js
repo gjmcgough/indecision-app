22a1b242d8e3c9292ad36ef575d581a33e5f289b
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-26 11:15:23 -0800

    install and import validator from yarn

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// // import './utils.js';
-// import subtract, { square, add } from './utils.js'
-//
-// console.log("App.js is running!!!");
-// console.log(square(4));
-// console.log(add(2,3));
-// console.log(subtract(10,2));
+// install > import > use
 
-import isSenior, {isAdult, canDrink} from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(64));
+import validator from 'validator';
+
+console.log(validator.isEmail('test@gmail.com'));

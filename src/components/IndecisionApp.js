3da22b5751f88252234a3781b0fe4306e514b326
greg.mcgough <greg.mcgough@servicenow.com>
2019-02-28 10:53:49 -0800

    set up settings file for scss and scss variables, styling is improving

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -63,19 +63,21 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle}/>
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-          options={this.state.options}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+            options={this.state.options}
+          />
+        </div>
       <OptionModal
         selectedOption={this.state.selectedOption}
         handleClearSelectedOption={this.handleClearSelectedOption}
